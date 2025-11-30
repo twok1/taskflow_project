b18d359d66f138e259ea4332792278b0b74927ff
twok1 <twokey25@gmail.com>
2025-11-30 17:39:48 +0300

    urls

---
+++ taskflow_project/tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'tasks'
+
+url_patterns = [
+    path('', views.project_list, name='project_list'),
+    path('project/<int:project_id>', views.project_detail, name='project_detail'),
+    path('task/<int:task_id', views.task_detail, name='task_detail')
+]
