8cd4db255b4fb9a9eeff95411f93c4c19d569d56
twok1 <twokey25@gmail.com>
2025-11-30 17:24:09 +0300

    adv admin

--- taskflow_project/tasks/admin.py
+++ taskflow_project/tasks/admin.py
@@ -1,17 +1,33 @@
 from django.contrib import admin
+from .models import Project, Task, Tag, Comment, TaskProgress, ProjectMembership
 
-from .models import (
-    Task,
-    Project, 
-    ProjectMembership,
-    Tag,
-    Comment,
-    TaskProgress
-)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['name', 'description']
 
-admin.site.register(Task)
-admin.site.register(Tag)
-admin.site.register(Project)
-admin.site.register(ProjectMembership)
-admin.site.register(Comment)
-admin.site.register(TaskProgress)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'project', 'status', 'priority', 'created_at']
+    list_filter = ['status', 'priority', 'created_at']
+    search_fields = ['title', 'description']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'color']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['task', 'author', 'created_at']
+    list_filter = ['created_at']
+
+@admin.register(TaskProgress)
+class TaskProgressAdmin(admin.ModelAdmin):
+    list_display = ['task', 'author', 'created_at']
+    list_filter = ['created_at']
+
+@admin.register(ProjectMembership)
+class ProjectMembershipAdmin(admin.ModelAdmin):
+    list_display = ['user', 'project', 'role', 'joined_at']
+    list_filter = ['role', 'joined_at']
