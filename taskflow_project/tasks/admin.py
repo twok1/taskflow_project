0bad18be497a03a031302eb820e8a77ec7a211ce
twok1 <twokey25@gmail.com>
2025-11-30 17:17:04 +0300

    admin

--- taskflow_project/tasks/admin.py
+++ taskflow_project/tasks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Task,
+    Project, 
+    ProjectMembership,
+    Tag,
+    Comment,
+    TaskProgress
+)
+
+admin.site.register(Task)
+admin.site.register(Tag)
+admin.site.register(Project)
+admin.site.register(ProjectMembership)
+admin.site.register(Comment)
+admin.site.register(TaskProgress)
